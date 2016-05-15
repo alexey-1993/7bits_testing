9f639f10486a3b9a9efaf9eb808dcff6e8b731a7
alexey-1993 <lefan-93@mail.ru>
2016-05-15 21:38:16 +0600

    dev::03::getters end setters

--- src/com/roon/MainClass.java
+++ src/com/roon/MainClass.java
@@ -14,15 +14,18 @@ public class MainClass {
         main.process("C:\\Users\\Алексей\\Downloads\\lefan_formatting\\lefan_formatting\\7bits_testing\\test.txt");
     }
 
+    public MainClass() {
+        setProcessor(new StringProcessorImpl());
+    }
+
     private void process(String filepath) {
-        processor = new StringProcessorImpl();
         File f = new File(filepath);
         StringBuilder text = new StringBuilder();
         try (BufferedReader fr = new BufferedReader(new FileReader(f))) {
             fr.lines().forEach(new Consumer<String>() {
                 @Override
                 public void accept(String s) {
-                    text.append(processor.processString(s));
+                    text.append(getProcessor().processString(s));
                 }
             });
 
@@ -32,6 +35,13 @@ public class MainClass {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public StringProcessor getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(StringProcessor processor) {
+        this.processor = processor;
     }
 }
